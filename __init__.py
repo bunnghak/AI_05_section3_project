46e25a81462942dffcc6d6c690b97ca49c61eb4f
전병학 <mintland@MintMini.local>
2021-10-13 18:04:18 +0900

    get keyword and seach then show result

--- __init__.py
+++ __init__.py
@@ -1,9 +1,15 @@
-from flask import Flask,render_template
-from AI_05_section3_project.LM.api_model import titles,img_list
+from flask import Flask,render_template,request
 app = Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def index():
-    thumbnail = img_list
-    title = titles
-    return render_template('index.html', thumbnail=thumbnail,title=title)
+    return render_template('index.html')
+
+@app.route('/result',methods=['GET'])
+def result(keyword=None):
+    temp = request.args.get('keyword')
+    word = str(temp)
+    from AI_05_section3_project.LM.api_model import youtube,search_from_api
+    url = youtube(word)
+    thumbnail,title,id_list= search_from_api(url)
+    return render_template('result.html', thumbnail=thumbnail,title=title,id=id_list)
